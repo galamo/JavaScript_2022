e80bc22e5e4b21f22095a0ab18f0f42fcceea63a
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-07-12 15:19:57 +0300

    functions

---
+++ Lesson_1/html/function.js
@@ -0,0 +1,13 @@
+const globalPassword = 999999;
+function login(userName, password = 999991) {
+    console.log(`Hello ${userName} , your
+     password is: ${password} , 
+     global password: ${globalPassword}`)
+    // console.log("Hello " + userName + " , how are you?") // old approach
+}
+console.log("before function execution")
+login("Shlomo@gmail.com", 12234434)
+login("Dorin@gmail.com", 1223444)
+login("ruth@gmail.com", 122334)
+login("neomi@gmail.com")
+console.log("after function execution")
---
+++ Lesson_1/html/index.js
@@ -0,0 +1,29 @@
+console.log("JS, Script is running")
+
+
+let userName = "galamo@gmail.com"
+let age = 33
+let address
+
+console.log(userName, age, address)
+
+console.log(typeof userName, typeof age, typeof address)
+
+userName = "dan@gov.mof.il"
+age = "33"
+
+console.log(userName, age, address)
+
+console.log(typeof userName, typeof age, typeof address)
+
+const url = "https://www.kikar.co.il/"
+
+// const url = "ggg" cannot 
+
+// let userName = "kki" // Cannot redeclare block-scoped variable 'userName'.ts(2451)
+// url = "https://www.facebook.co.il/" Uncaught SyntaxError: Identifier 'url' has already been declared (at index.js:21:7)
+
+
+console.log("JS, Script finished")
+
+
