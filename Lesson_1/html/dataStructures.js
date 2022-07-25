9cfe93d9cd9a1b90a0751b5e54e0824aca00ccf5
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-07-25 15:10:05 +0300

    delete

--- Lesson_1/html/dataStructures.js
+++ Lesson_1/html/dataStructures.js
@@ -41,14 +41,14 @@ users.forEach(function (item) { console.log(item) })
 const myUser = {
     name: "Gal",
     lastName: "Amouyal",
-    favoritesMeals: ["Pasta", "Pizza", "ViznizHalla"],
+    favoritesMeals: ["Pasta", "Pizza", "ViznizHalla", "Pizza"],
     address: { city: "Ashdod", street: "elizer", num: 11 },
     age: 33,
     vacationsWithoutChildren: [
         { location: "sayshel", nights: 5 },
-        { location: "eilat", nights: 4 },
-        { location: "usa", nights: 11 },
-        { location: "odesa", nights: 3 },
+        { place: "eilat", nights: 4 },
+        { destination: "usa", nights: 11 },
+        { place: "odesa", nights: 3 },
     ]
 }
 
@@ -63,20 +63,49 @@ function showUserDetails() {
     const h1UserName = document.createElement("h1")
     // insert fullName into h1
     h1UserName.innerText = fullName
-    // present the h1 inside the div
-    const h2 = document.createElement("h3")
-    h2.innerText = myUser.age;
-
-    userDetailsDiv.append(h1UserName, h2)
-
+    // create element for age
+    const h3 = document.createElement("h3")
+    // insert age into h3
+    h3.innerText = myUser.age;
+    // create favorites header
+    const h3FavoritesHeader = document.createElement("h3")
+    h3FavoritesHeader.innerText = "Favorites";
+    // draw the elements into the userDetailsDiv
+    userDetailsDiv.append(h1UserName, h3, h3FavoritesHeader)
+    // draw favorites 
     for (let index = 0; index < myUser.favoritesMeals.length; index++) {
         const currentMeal = myUser.favoritesMeals[index];
         const currentFavorite = document.createElement("li")
-        currentFavorite.innerText = currentMeal;
-        currentFavorite.style.color = "pink"
+        currentFavorite.className = "list-group-item"
+        currentFavorite.innerText = currentMeal
+        currentFavorite.id = currentMeal
+        const deleteButton = document.createElement("button")
+        deleteButton.className = "btn btn-default"
+        deleteButton.innerText = "🗑️"
+        deleteButton.addEventListener("click", function () {
+            console.log(this.parentElement.innerText)
+            if (this.parentElement.id !== "Pizza") {
+                this.parentElement.remove()
+            }
+
+        })
+        currentFavorite.style.color = "lightblue"
+        currentFavorite.append(deleteButton)
         userDetailsDiv.append(currentFavorite)
     }
-
+    // create vacations header
+    const h3VacationsHeader = document.createElement("h3")
+    h3VacationsHeader.innerText = "Vacations";
+    userDetailsDiv.append(h3VacationsHeader)
+    // draw vacations 
+    for (let index = 0; index < myUser.vacationsWithoutChildren.length; index++) {
+        const vacation = myUser.vacationsWithoutChildren[index];
+        const currentVacation = document.createElement("li")
+        const keys = Object.keys(myUser.vacationsWithoutChildren[index])
+        currentVacation.innerText = `${keys[0]}: ${vacation[keys[0]]} ${keys[1]}: ${vacation[keys[1]]}`
+        currentVacation.style.color = "lightblue"
+        userDetailsDiv.append(currentVacation)
+    }
 
 
 
