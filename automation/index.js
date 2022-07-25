83a5420cdec275fbaae6bc3a7ee9b14ad042815b
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-07-25 09:10:02 +0300

    automation

---
+++ automation/index.js
@@ -0,0 +1,13 @@
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
+
+(async function example() {
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    try {
+        await driver.get('https://www.kikar.co.il/');
+        const ele = await driver.findElement(By.className('mainart_special_title'));
+        console.log(ele)
+        // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+        await driver.quit();
+    }
+})();
