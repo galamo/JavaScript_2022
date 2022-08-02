86b80d3c9c7591b272c1a95c0fba3f5035688f76
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-08-02 15:35:09 +0300

    node

--- automation/index.js
+++ automation/index.js
@@ -3,12 +3,21 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
 (async function example() {
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
-        await driver.get('https://www.kikar.co.il/');
-        const ele = await driver.findElement(By.className('mainart_special_title'));
-        ele.click()
-        console.log(ele)
+        await driver.get('https://gemelnet.cma.gov.il/views/dafmakdim.aspx');
+        await new Promise(r => setTimeout(r, 3000));
+        const buttonKnisa = await driver.findElement(By.id('knisa'));
+        buttonKnisa.click()
+        await new Promise(r => setTimeout(r, 3000));
+        // await driver.findElement("//option[@value='0000']").click()
+        const btnAdd = await driver.findElement(By.id('btnAdd'));
+        btnAdd.click()
+
+
+
         // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
     } finally {
-        // await driver.quit();
+        setTimeout(async () => {
+            await driver.quit();
+        }, 5000);
     }
 })();
