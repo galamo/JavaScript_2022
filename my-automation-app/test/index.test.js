e8f68980e85e42b8e49a67779232fad9b112b5ee
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-08-09 15:06:57 +0300

    test

--- my-automation-app/test/index.test.js
+++ my-automation-app/test/index.test.js
@@ -19,30 +19,26 @@ describe("Budget Application - Hatzmadot", function () {
         // Creating the Browser object
         let driver = await new Builder().forBrowser(Browser.CHROME).build();
         try {
-            // Requesting the Web application URL ( GET )
+            // Browsing the Budget
             await driver.get(budgetUrl);
             // Collect the breadCrumbs from the site
-            await new Promise(r => setTimeout(r, 3000));
-            const breadCrumbs = await driver.findElement(By.className('tw-text-nav-title-page'));
-            // Extract the text from the breadCrumbs element
+            const breadCrumbs = await driver.wait(until.elementLocated(By.className('tw-text-nav-title-page')));
             const text = await breadCrumbs.getText()
             expect(text).to.be.equal('ניהול הצמדות > ניהול גרסאות הצמדה')
 
-            const sideBarButton = await driver.findElement(By.className('fix-position-icon tw-w-42px tw-h-35px tw-pr-[7px] tw-select-none tw-z-999'));
-            // Assert the string to the correct header
+            const sideBarButton = await driver.wait(until.elementLocated(By.className('fix-position-icon tw-w-42px tw-h-35px tw-pr-[7px] tw-select-none tw-z-999')));
             sideBarButton.click()
-            await new Promise(r => setTimeout(r, 3000));
-            const mahaduraNew = await driver.findElement(By.className('text-appear-animation tw-bg-side-menu-button tw-h-50px tw-leading-sub-menu fix-sub-menu-postion'))
+
+            const mahaduraNew = await driver.wait(until.elementLocated(By.className('text-appear-animation tw-bg-side-menu-button tw-h-50px tw-leading-sub-menu fix-sub-menu-postion')));
             mahaduraNew.click()
-            await new Promise(r => setTimeout(r, 3000));
-            const breadCrumbs2 = await driver.findElement(By.className('tw-text-nav-title-page'));
-            // Extract the text from the breadCrumbs element
+
+            const breadCrumbs2 = await driver.wait(until.elementLocated(By.className('tw-text-nav-title-page')));
             const text2 = await breadCrumbs2.getText()
             expect(text2).to.be.equal('בית > מהדורה')
         } finally {
             setTimeout(async () => {
                 await driver.quit();
-            }, 10000);
+            }, 3000);
         }
     })
 })
