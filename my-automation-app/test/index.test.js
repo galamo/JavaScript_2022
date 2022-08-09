bd1f81d2d2edae93c4847d2204574530de4826aa
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-08-09 14:33:39 +0300

    a

--- my-automation-app/test/index.test.js
+++ my-automation-app/test/index.test.js
@@ -21,10 +21,29 @@ describe("Budget Application - Hatzmadot", function () {
         try {
             // Requesting the Web application URL ( GET )
             await driver.get(budgetUrl);
-        } catch (ex) {
+            // Collect the breadCrumbs from the site
+            await new Promise(r => setTimeout(r, 3000));
+            const breadCrumbs = await driver.findElement(By.className('tw-text-nav-title-page'));
+            // Extract the text from the breadCrumbs element
+            const text = await breadCrumbs.getText()
+            expect(text).to.be.equal('ניהול הצמדות > ניהול גרסאות הצמדה')
+
+            const sideBarButton = await driver.findElement(By.className('fix-position-icon tw-w-42px tw-h-35px tw-pr-[7px] tw-select-none tw-z-999'));
+            // Assert the string to the correct header
+            sideBarButton.click()
+            await new Promise(r => setTimeout(r, 3000));
+            const mahaduraNew = await driver.findElement(By.className('text-appear-animation tw-bg-side-menu-button tw-h-50px tw-leading-sub-menu fix-sub-menu-postion'))
+            mahaduraNew.click()
+            await new Promise(r => setTimeout(r, 3000));
+            const breadCrumbs2 = await driver.findElement(By.className('tw-text-nav-title-page'));
+            // Extract the text from the breadCrumbs element
+            const text2 = await breadCrumbs2.getText()
+            expect(text2).to.be.equal('בית > מהדורה')
+        } finally {
             setTimeout(async () => {
                 await driver.quit();
-            }, 5000);
+            }, 10000);
         }
     })
 })
+
