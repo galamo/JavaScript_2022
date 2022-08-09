7fdd424ef12a591364ae8d66879b8399e743506c
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-08-09 16:03:09 +0300

    a

--- my-automation-app/test/index.test.js
+++ my-automation-app/test/index.test.js
@@ -28,17 +28,56 @@ describe("Budget Application - Hatzmadot", function () {
 
             const sideBarButton = await driver.wait(until.elementLocated(By.className('fix-position-icon tw-w-42px tw-h-35px tw-pr-[7px] tw-select-none tw-z-999')));
             sideBarButton.click()
-
+            await new Promise(r => setTimeout(r, 2000));
             const mahaduraNew = await driver.wait(until.elementLocated(By.className('text-appear-animation tw-bg-side-menu-button tw-h-50px tw-leading-sub-menu fix-sub-menu-postion')));
             mahaduraNew.click()
-
+            await new Promise(r => setTimeout(r, 2000));
             const breadCrumbs2 = await driver.wait(until.elementLocated(By.className('tw-text-nav-title-page')));
             const text2 = await breadCrumbs2.getText()
             expect(text2).to.be.equal('בית > מהדורה')
+
+            const sideBarButton2 = await driver.wait(until.elementLocated(By.xpath('/html/body/app-root/div[2]/div[1]/gov-side-menu/div/div/div[5]/div/div')));
+            sideBarButton2.click()
+
+            const sideBarClickCodes = await driver.wait(until.elementLocated(By.xpath('/html/body/app-root/div[2]/div[1]/gov-side-menu/div/div/div[5]/div/div[2]/div[2]/div[2]/span/div')));
+            sideBarClickCodes.click()
+
+            await new Promise(r => setTimeout(r, 2000));
+            const breadCrumbs3 = await driver.wait(until.elementLocated(By.className('tw-text-nav-title-page')));
+            const text3 = await breadCrumbs3.getText()
+            expect(text3).to.be.equal('ניהול הצמדות > מקדמי מחירים')
         } finally {
             setTimeout(async () => {
                 await driver.quit();
             }, 3000);
+        }
+    })
+    it("New Girsa Hatzmada", async () => {
+        let driver = await new Builder().forBrowser(Browser.CHROME).build();
+        try {
+            await driver.get(budgetUrl);
+            await new Promise(r => setTimeout(r, 2000));
+            const buttonAdd = await driver.findElement(By.className("p-element btn-color p-button p-component ng-star-inserted"));
+            buttonAdd.click()
+            await new Promise(r => setTimeout(r, 2000));
+            const elements = await driver.findElements(By.className('p-inputtext p-component p-element ng-untouched ng-dirty ng-invalid ng-star-inserted'))
+            elements[0].sendKeys(`automation_girsa_${new Date().getTime()}`)
+            elements[1].sendKeys("8")
+            // const edit = await driver.findElement(By.className('p-button-icon pi pi-pencil'))
+            // edit.click()
+            await new Promise(r => setTimeout(r, 1000));
+            const save = await driver.findElement(By.className('p-button-icon pi pi-check'))
+            save.click();
+            await new Promise(r => setTimeout(r, 1500));
+            const close = await driver.findElement(By.className('p-toast-icon-close-icon pi pi-times'))
+            const isElementExist = close !== null
+            close.click()
+            expect(isElementExist).to.be.equal(true)
+            // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+        } finally {
+            setTimeout(async () => {
+                await driver.quit();
+            }, 5000);
         }
     })
 })
